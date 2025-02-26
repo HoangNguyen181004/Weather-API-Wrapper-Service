8b2a4af8115dd5f71380d0fe99ba58424f960c75
HoangNguyen181004 <hhoang18100401@gmail.com>
2025-02-27 01:10:56 +0700

    Add all project files

---
+++ src/test/java/com/example/WeatherAPI_Service/WeatherApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WeatherAPI_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
